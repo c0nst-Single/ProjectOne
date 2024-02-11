818865a00f78ac3d196a9b2574fafd6d89fd1f41
unknown <zpezzigl@gmail.com>
2024-02-11 22:55:11 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,67 @@
-
 let gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')),
+const sass = require('gulp-sass')(require('sass'));
 rename = require('gulp-rename');
 const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src('app/scss/**/*.scss') // !return gulp.src('app/scss/style.scss')
             .pipe(sass({outputStyle: 'compressed'}))
             .pipe(rename({suffix : '.min'}))
+            .pipe(autoprefixer({
+                overrideBrowserslist:['last 8 versions']
+            }))
             .pipe(gulp.dest('app/css'))
             .pipe(browserSync.reload({stream: true}))
-});  
+});
+
+gulp.task('style', function(){
+    return gulp.src([
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/magnific-popup/dist/magnific-popup.css'
+    ])
+    .pipe(concat('libs.min.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('app/css'))
+})
+
+gulp.task('script', function(){
+    return gulp.src([
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+})
 
 gulp.task('html', function(){
     return gulp.src('app/*.html')
-    .pipe((browserSync.reload({stream: true})))
+    .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('js', function(){
     return gulp.src('app/js/*.js')
-    .pipe((browserSync.reload({stream: true})))
+    .pipe(browserSync.reload({stream: true}))
 });
+
 
 gulp.task('browser-sync', function() {
     browserSync.init({
-        proxy: "app/"
+        server: {
+            baseDir: "app/"
+        }
     });
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
-    gulp.watch('app/*.html', gulp.parallel('html'))
-    gulp.watch('app/ls/*.js', gulp.parallel('js'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('html'));
+    gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
 
-gulp.task('default', gulp.parallel('sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('style','sass', 'watch', 'browser-sync'))
